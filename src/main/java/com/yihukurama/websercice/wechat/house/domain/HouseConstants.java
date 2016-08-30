562af8806f759ee501ea9a660f1859f1bfae658a
dengshuai <479757405@qq.com>
2016-08-30 21:47:33 +0800

    初始化微信参数

--- src/main/java/com/yihukurama/websercice/wechat/house/domain/HouseConstants.java
+++ src/main/java/com/yihukurama/websercice/wechat/house/domain/HouseConstants.java
@@ -10,9 +10,9 @@ package com.yihukurama.websercice.wechat.house.domain;
  * @author Dengshuai
  */
 public class HouseConstants {
-	public static final String TOKEN = "yihukurama";
-    public static final String APPID = "wxeb2343c9f831f559";
-    public static final String APPSECRET = "98946cb68f1e3c88855c053b1d881dbb";
+    public static final String TOKEN = "yihukurama";
+    public static final String APPID = "wxa048dc9d8061f765";
+    public static final String APPSECRET = "740b8c8be7fd89c12334a383343276eb";
     public static String accessToken = "vSVeAwQr879Z5qKA5kTAsZlm0Qd6ABR7K3lpy1f-DPlkdxwlU9DvAQbRbyAsK-vfbUhoQuV8ShFWzQ4Jh8EbtLN5m1w6OQkq1Qh9LhUiRq4FYOAXzvfBVTNQ8Odk5rjUYBVjAHACFZ";
-
+    public static final String AESKEY = "2sEDFpfQDUSg9kmE5w9X4fXwsPrTEhMDVZwVP5Plpju";
 }
