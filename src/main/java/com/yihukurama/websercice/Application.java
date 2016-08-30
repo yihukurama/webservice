a721ae83787b0c7c54926d90f9aca9ea8537a5fb
Administrator <479757405@qq.com>
2016-08-30 23:15:25 +0800

    加入微店

--- src/main/java/com/yihukurama/websercice/Application.java
+++ src/main/java/com/yihukurama/websercice/Application.java
@@ -12,7 +12,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
